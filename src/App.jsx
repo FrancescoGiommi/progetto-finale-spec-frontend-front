b5af9565ba2606b6182690906055066c37348d59
Francesco <you@example.com>
2025-04-09 12:33:04 +0200

    Add videogamesListPage

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
+import VideogamesListPage from "./pages/videogamesListPage";
+
 import { useState } from "react";
 
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return <></>;
 }
 
---
+++ src/pages/videogamesListPage.jsx
@@ -0,0 +1,3 @@
+export default function VideogamesListPage() {
+  return <h1>Lista videogiochi</h1>;
+}
