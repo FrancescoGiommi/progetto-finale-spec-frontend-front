1e57ba6448cc51ae67b5cf8125472d3e5d5960cd
Francesco <you@example.com>
2025-04-09 16:18:00 +0200

    Add customHook useGames

--- src/customHooks/useGames.js
+++ src/customHooks/useGames.js
@@ -1,3 +1,27 @@
-export defult function useGames() {
-    
+import { useEffect, useState } from "react";
+
+export default function useGames() {
+  /* Lista dei giochi */
+  const [gamesList, setGamesList] = useState([]);
+  const gamesUrl = import.meta.env.VITE_BACKEND_URL;
+
+  console.log("gamesUrl:", import.meta.env.VITE_BACKEND_URL);
+
+  const gamesFetch = async () => {
+    try {
+      const response = await fetch(`${gamesUrl}/videogames`);
+      const data = await response.json();
+      if (!response.ok) {
+        throw new Error("Errore nella risposta del server");
+      }
+      setGamesList(data);
+      console.log(data);
+    } catch (error) {
+      console.error("Errore durante il fetch dei giochi:", error.message);
+    }
+  };
+
+  useEffect(() => {
+    gamesFetch();
+  }, []);
 }
